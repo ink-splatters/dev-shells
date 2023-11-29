c99a6e85d729a1bff9f81cbe7c911f0adf3371a2
Peter A. <ink.splatters@pm.me>
2023-11-29 01:39:40 +0100

    progress

--- flake.nix
+++ flake.nix
@@ -1,5 +1,8 @@
 {
-
+  inputs = {
+	nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+	devenv.url = "github:cachix/devenv";
+  }
   nixConfig = {
     extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
@@ -10,6 +13,10 @@
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
+      formatter = forEachSystem (system: {
+		nixpkgs.nixpkgs-fmt;
+	});
+
       packages = forEachSystem (system: {
         devenv-up = self.devShells.${system}.default.config.procfileScript;
       });
