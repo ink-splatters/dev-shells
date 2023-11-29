88d772b246e8160947e084a5404224a251d7de84
Peter A. <ink.splatters@pm.me>
2023-11-29 01:40:18 +0100

    * pre-commit fix * formatting

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
   inputs = {
-	nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-	devenv.url = "github:cachix/devenv";
-  }
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+
   nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
+    extra-substituters = [ https://devenv.cachix.org https://nix-community.cachix.org ];
+    extra-trusted-public-keys = [ devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= ];
   };
 
   outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
@@ -13,9 +14,9 @@
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      formatter = forEachSystem (system: {
-		nixpkgs.nixpkgs-fmt;
-	});
+      formatter = forEachSystem (system:
+        nixpkgs.legacyPackages.${system}.nixpkgs-fmt
+      );
 
       packages = forEachSystem (system: {
         devenv-up = self.devShells.${system}.default.config.procfileScript;
