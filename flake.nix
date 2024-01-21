572bd8fe42e9cf9d15686e1a2e47774c8e39af9c
Peter A. <ink.splatters@pm.me>
2024-01-21 21:09:36 +0100

    merge to one flake

--- flake.nix
+++ flake.nix
@@ -10,16 +10,36 @@
       in
       with pkgs; {
         formatter = nixpkgs-fmt;
-        devShells.default = mkShell.override { stdenv = stdenv; } {
+        devShells = {
+          swift = mkShell.override { stdenv = stdenv; } {
 
-          hardeningDisable = [ "all" ];
+            nativeBuildInputs = [
+              swift
+              swiftpm
+              xcodebuild
+            ];
 
-          CFLAGS = "-mcpu native -O3";
-          CXXFLAGS = "-mcpu native -stdlib=libc++ -O3";
-          shellHook = ''
-            export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
-          '';
+            hardeningDisable = [ "all" ];
+
+            CXXFLAGS = "-mcpu native -O3";
+            shellHook = ''
+              export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
+            '';
+          };
+
+
+          cpp = mkShell.override { stdenv = stdenv; } {
+
+            hardeningDisable = [ "all" ];
+
+            CFLAGS = "-mcpu native -O3";
+            CXXFLAGS = "-mcpu native -stdlib=libc++ -O3";
+            shellHook = ''
+              export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
+            '';
+          };
         };
+
         checks.default = stdenv.mkDerivation {
           inherit (self.devShells.${system}.default) hardeningDisable CFLAGS CXXFLAGS;
 
