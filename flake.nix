6d7f9221516e2bbd4a4560e890e28d0693647042
Peter A. <ink.splatters@pm.me>
2024-01-21 17:32:57 +0100

    basic cpp shell

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
-  description = "A basic flake with a shell";
+  description = "basic cpp development shell";
 
   outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs.llvmPackages_latest) stdenv;
+        inherit (pkgs) nixpkgs-fmt;
       in
-      {
-        devShells.default = pkgs.mkShell {
-          # packages = [ pkgs.bashInteractive ];
-
-          shellHook=''
+      with pkgs; {
+        formatter = nixpkgs-fmt;
+        devShells.default = mkShell.override { stdenv = stdenv; } {
+          CFLAGS = "-mcpu native";
+          CXXFLAGS = "-mcpu native -stdlib=libc++";
+          shellHook = ''
             export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
           '';
         };
