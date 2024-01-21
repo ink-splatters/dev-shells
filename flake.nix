9b8825c4dec34f2b7ea080eca4277f85dce596dc
Peter A. <ink.splatters@pm.me>
2024-01-21 17:37:56 +0100

    hardening disable + O3

--- flake.nix
+++ flake.nix
@@ -11,11 +11,16 @@
       with pkgs; {
         formatter = nixpkgs-fmt;
         devShells.default = mkShell.override { stdenv = stdenv; } {
-          CFLAGS = "-mcpu native";
-          CXXFLAGS = "-mcpu native -stdlib=libc++";
+
+          hardeningDisable = [ "all" ];
+
+          CFLAGS = "-mcpu native -O3";
+          CXXFLAGS = "-mcpu native -stdlib=libc++ -O3";
           shellHook = ''
             export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
           '';
+
+
         };
       });
 }
