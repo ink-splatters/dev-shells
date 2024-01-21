0600c818805f0dfcebd7ac49d19a1a23a55b03d8
Peter A. <ink.splatters@pm.me>
2024-01-21 18:14:22 +0100

    nix flake check: compile and run helloworld cpp

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "basic cpp development shell";
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, self, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -19,8 +19,21 @@
           shellHook = ''
             export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
           '';
+        };
+        checks.default = stdenv.mkDerivation {
+          inherit (self.devShells.${system}.default) hardeningDisable CFLAGS CXXFLAGS;
 
+          name = "check";
+          src = ./.;
+          dontBuild = true;
+          doCheck = true;
 
+          checkPhase = ''
+            clang++ checks/main.cpp -o helloworld
+          '';
+          installPhase = ''
+            mkdir "$out"
+          '';
         };
       });
 }
